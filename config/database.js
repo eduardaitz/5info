bf7ab07d1c19c29a33a85fd038d7227e4880dcda
eduardaitz <mariagoncalves.bg058@academico.ifsul.edu.br>
2022-02-18 14:53:02 -0300

    primeiro commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://eduardaitz:mmadusk8123DUDA@cluster0.wk9hs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+module.exports = mongoose;
